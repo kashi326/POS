2f32231f4db9c5f87e4d0973ba133e604cd8dd1d
kashi326 <kashifahmad0332@gmail.com>
2020-06-28 17:56:58 +0500

    Login page and redirection to Home page

---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Home() {
+  return (
+    <Link to='/'>
+        <h3>
+          Sign Out</h3>
+      </Link>
+  );
+}
+
+export default Home;
